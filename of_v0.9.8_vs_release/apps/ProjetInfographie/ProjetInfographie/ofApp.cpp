1d92bd575cb8c5dd22c22faa27997513ae37bb7c
Unknown <alexandre.mccune@gmail.com>
2017-03-06 21:50:17 -0500

    UI fait avec ofxGui

--- of_v0.9.8_vs_release/apps/ProjetInfographie/ProjetInfographie/ofApp.cpp
+++ of_v0.9.8_vs_release/apps/ProjetInfographie/ProjetInfographie/ofApp.cpp
@@ -8,7 +8,30 @@ ofApp::ofApp()
 //--------------------------------------------------------------
 void ofApp::setup()
 {
-	ofSetWindowTitle("Top 10 Album of all time by the rolling stone magazine!");
+	ofSetWindowTitle("Visualiseur interactif de scènes 3D");
+
+	initPosition();
+	initDimension();
+	initColors();
+	initGroups();
+
+	gui.setDefaultWidth(270);
+	gui.setup();
+
+	is2dDisplay = false;
+	is3dDisplay = false;
+	isPropertiesDisplay = false;
+	isListenersUnlocked = true;
+
+	btnSelect.addListener(this, &ofApp::btnSelectClicked);
+	btn2D.addListener(this, &ofApp::btn2DClicked);
+	btn3D.addListener(this, &ofApp::btn3DClicked);
+	btnProperty.addListener(this, &ofApp::btnPropertyClicked);
+	btnDraw.addListener(this, &ofApp::btnDrawClicked);
+	btnExit.addListener(this, &ofApp::btnExitClicked);
+
+	setupGui();
+
 
 	rend = new renderer();
 	rend->setup();
@@ -19,6 +42,8 @@ void ofApp::setup()
 //--------------------------------------------------------------
 void ofApp::update()
 {
+	setColors();
+	rend->background = background;
 	rend->update();
 }
 
@@ -26,6 +51,7 @@ void ofApp::update()
 void ofApp::draw()
 {
 	rend->draw();
+	gui.draw();
 }
 
 void ofApp::exit()
@@ -112,4 +138,298 @@ void ofApp::gotMessage(ofMessage msg) {
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo) {
 
+}
+
+void ofApp::setupGui() {
+	
+	isListenersUnlocked = false;
+
+	gui.clear(); 
+
+	gui.add(ofParameter<string>("Boite a outil"));
+	gui.add(btnDraw.setup("Dessiner!"));
+	gui.add(btnSelect.setup("Outils de selection"));
+	gui.add(btn2D.setup("Outils de dessin 2D"));
+	gui.add(btn3D.setup("Outils de dessin 3D"));
+	gui.add(btnProperty.setup("Proprietes de l'outil de dessin"));
+	
+	if (is2dDisplay) {
+		display2D();
+	}
+
+	if (is3dDisplay) {
+		display3D();
+	}
+
+	if (isPropertiesDisplay) {
+		displayProperties();
+	}
+	
+	gui.add(btnExit.setup("Quitter"));
+
+	isListenersUnlocked = true;
+}
+
+void ofApp::displayProperties() 
+{
+	ofColor defaultColor = gui.getFillColor();
+
+	gui.add(groupThick);
+	
+	gui.setDefaultFillColor(fill);
+	gui.add(groupFill);
+
+	gui.setDefaultFillColor(stroke);
+	gui.add(groupStroke);
+
+	gui.setDefaultFillColor(background);
+	gui.add(groupBackground);
+
+	gui.setDefaultFillColor(defaultColor);
+	
+}
+
+void ofApp::display2D()
+{
+	float centreX = (MaxX + MinX) / 2;
+	float centreY = (MaxY + MinY) / 2;
+
+	gui.add(ofParameter<string>("--------------------------------"));
+	gui.add(ofParameter<string>("Position"));
+	gui.add(posX.set(posX));
+	gui.add(posY.set(posY));
+	gui.add(ofParameter<string>("--------------------------------"));
+	gui.add(ofParameter<string>("Dimension"));
+	gui.add(width.set(width));
+	gui.add(height.set(height));
+}
+
+void ofApp::display3D()
+{
+	gui.add(groupPosition);
+
+	gui.add(groupDimension);
+}
+
+void ofApp::btnSelectClicked()
+{
+	if(isListenersUnlocked)
+	{
+		isPropertiesDisplay = false;
+		is2dDisplay = false;
+		is3dDisplay = false;
+
+		setupGui();
+	}
+}
+
+void ofApp::btnPropertyClicked()
+{
+	if (isListenersUnlocked)
+	{
+		ofLog() << "<app::btnPropertyClicked>";
+
+		isPropertiesDisplay = !isPropertiesDisplay;
+		setupGui();
+	}
+}
+
+void ofApp::btn2DClicked()
+{
+	if (isListenersUnlocked)
+	{
+		ofLog() << "<app::btn2DClicked>";
+
+		is2dDisplay = !is2dDisplay;
+
+		is3dDisplay = false;
+
+		setupGui();
+	}
+}
+
+void ofApp::btn3DClicked()
+{
+	if (isListenersUnlocked)
+	{
+		ofLog() << "<app::btn3DClicked>";
+
+		is3dDisplay = !is3dDisplay;
+
+		is2dDisplay = false;
+
+		setupGui();
+	}
+}
+
+void ofApp::btnDrawClicked()
+{
+	if (isListenersUnlocked)
+	{
+		ofLog() << "<app::btnDrawClicked>";
+
+		//TODO: Faites vous du fun!
+
+		rend->createCube(posX, posY, posZ, height, stroke);
+	}
+}
+
+void ofApp::btnExitClicked() 
+{
+	if (isListenersUnlocked)
+	{
+		ofLog() << "<app::btnExitClicked>";
+		exit();		//TODO: Trouver une autre solution
+	}
+}
+
+void ofApp::initGroups()
+{
+
+	groupPosition.add(ofParameter<string>("Position"));
+	groupPosition.add(posX.set(posX));
+	groupPosition.add(posY.set(posY));
+	groupPosition.add(posZ.set(posZ));
+
+	groupDimension.add(ofParameter<string>("Dimension"));
+	groupDimension.add(width.set(width));
+	groupDimension.add(height.set(height));
+	groupDimension.add(depth.set(depth));
+
+	groupThick.add(ofParameter<string>("Epaisseur des traits"));
+	groupThick.add(strokeThickness.set(strokeThickness));
+
+	groupFill.add(ofParameter<string>("Couleur de remplissage"));
+	groupFill.add(fillHue.set(fillHue));
+	groupFill.add(fillSaturation.set(fillSaturation));
+	groupFill.add(fillBrightess.set(fillBrightess));
+	groupFill.add(fillAlpha.set(fillAlpha));
+		
+	groupStroke.add(ofParameter<string>("Couleur de bordure"));
+	groupStroke.add(strokeHue.set(strokeHue));
+	groupStroke.add(strokeSaturation.set(strokeSaturation));
+	groupStroke.add(strokeBrightess.set(strokeBrightess));
+	groupStroke.add(strokeAlpha.set(strokeAlpha));
+
+	groupBackground.add(ofParameter<string>("Couleur de fond"));
+	groupBackground.add(bgHue.set(bgHue));
+	groupBackground.add(bgSaturation.set(bgSaturation));
+	groupBackground.add(bgBrightess.set(bgBrightess));
+ 
+}
+
+void ofApp::initPosition() {
+
+	posX.setName("X");
+	posX.setMin(MinX);
+	posX.setMax(MaxX);
+	posX.set((MinX + MaxX) / 2);
+
+	posY.setName("Y");
+	posY.setMin(MinY);
+	posY.setMax(MaxY);
+	posY.set((MinY + MaxY) / 2);
+
+	posZ.setName("Z");
+	posZ.setMin(MinZ);
+	posZ.setMax(MaxZ);
+	posZ.set((MinZ + MaxZ) / 2);
+}
+
+void ofApp::initDimension() 
+{
+	height.setName("Hauteur");
+	height.setMin(MinY);
+	height.setMax(MaxY);
+	height.set((MinY + MaxY) / 2);
+
+	width.setName("Largeur");
+	width.setMin(MinX);
+	width.setMax(MaxX);
+	width.set((MinX + MaxX) / 2);
+
+	depth.setName("Profondeur");
+	depth.setMin(MinZ);
+	depth.setMax(MaxZ);
+	depth.set((MinZ + MaxZ) / 2);
+
+	radius.setName("Rayon");
+	radius.setMin(MinY);
+	radius.setMax(MaxY);
+	radius.set((MinY + MaxY) / 2);
+
+
+	strokeThickness.setName("Epaisseur");
+	strokeThickness.setMin(0);
+	strokeThickness.setMax(100);
+	strokeThickness.set(10);
+}
+
+void ofApp::initColors()
+{
+	
+	fillHue.setName("Teinte");
+	fillHue.setMin(0);
+	fillHue.setMax(255);
+	fillHue.set(0);
+
+	fillSaturation.setName("Saturation");
+	fillSaturation.setMin(0);
+	fillSaturation.setMax(255);
+	fillSaturation.set(100);
+	
+	fillBrightess.setName("Valeur");
+	fillBrightess.setMin(0);
+	fillBrightess.setMax(255);
+	fillBrightess.set(255);
+
+	fillAlpha.setName("Transparence");
+	fillAlpha.setMin(0);
+	fillAlpha.setMax(255);
+	fillAlpha.set(255);
+
+
+	strokeHue.setName("Teinte");
+	strokeHue.setMin(0);
+	strokeHue.setMax(255);
+	strokeHue.set(0);
+
+	strokeSaturation.setName("Saturation");
+	strokeSaturation.setMin(0);
+	strokeSaturation.setMax(255);
+	strokeSaturation.set(100);
+
+	strokeBrightess.setName("Valeur");
+	strokeBrightess.setMin(0);
+	strokeBrightess.setMax(255);
+	strokeBrightess.set(255);
+
+	strokeAlpha.setName("Transparence");
+	strokeAlpha.setMin(0);
+	strokeAlpha.setMax(255);
+	strokeAlpha.set(255);
+
+
+	bgHue.setName("Teinte");
+	bgHue.setMin(0);
+	bgHue.setMax(255);
+	bgHue.set(170);
+
+	bgSaturation.setName("Saturation");
+	bgSaturation.setMin(0);
+	bgSaturation.setMax(255);
+	bgSaturation.set(170);
+
+	bgBrightess.setName("Valeur");
+	bgBrightess.setMin(0);
+	bgBrightess.setMax(255);
+	bgBrightess.set(170);
+
+}
+
+void ofApp::setColors()
+{
+	stroke = ofColor::fromHsb(strokeHue, strokeSaturation, strokeBrightess, strokeAlpha);
+	fill = ofColor::fromHsb(fillHue, fillSaturation, fillBrightess, fillAlpha);
+	background = ofColor::fromHsb(bgHue, bgSaturation, bgBrightess);
 }
--- of_v0.9.8_vs_release/apps/ProjetInfographie/ProjetInfographie/ofApp.h
+++ of_v0.9.8_vs_release/apps/ProjetInfographie/ProjetInfographie/ofApp.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 #include "renderer.h"
 
 class ofApp : public ofBaseApp {
 
 public:
+
+	const float MaxX = 2000, MaxY = 1000, MaxZ = 1000;
+	const float MinX = 0, MinY = 0, MinZ = -1000;
 
 	renderer * rend;
 
@@ -30,5 +34,80 @@ public:
 	void windowResized(int w, int h);
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
+
+
+private:
+
+	bool isListenersUnlocked;
+
+	ofxPanel gui;
+	ofxButton btn2D;
+	ofxButton btn3D;
+	ofxButton btnSelect;
+	ofxButton btnProperty;
+	ofxButton btnDraw;
+	ofxButton btnExit;
+
+	int i = 0;
+
+	bool isPropertiesDisplay;
+	bool is2dDisplay;
+	bool is3dDisplay;
+	
+
+	ofParameterGroup groupPosition;
+	ofParameter<float> posX;
+	ofParameter<float> posY;
+	ofParameter<float> posZ;
+
+	ofParameterGroup groupDimension;
+	ofParameter<float> height;
+	ofParameter<float> width;
+	ofParameter<float> depth;
+	ofParameter<float> radius;
+
+
+
+	ofParameterGroup groupThick;
+	ofParameter<float> strokeThickness;
+
+	ofParameterGroup groupStroke;
+	ofColor stroke;
+	ofParameter<float> strokeHue;
+	ofParameter<float> strokeSaturation;
+	ofParameter<float> strokeBrightess;
+	ofParameter<float> strokeAlpha;
+
+	ofParameterGroup groupFill;
+	ofColor fill;
+	ofParameter<float> fillHue;
+	ofParameter<float> fillSaturation;
+	ofParameter<float> fillBrightess;
+	ofParameter<float> fillAlpha;
+
+	ofParameterGroup groupBackground;
+	ofColor background;
+	ofParameter<float> bgHue;
+	ofParameter<float> bgSaturation;
+	ofParameter<float> bgBrightess;
+
+	void initGroups();
+
+	void initPosition();
+	void initDimension();
+	void initColors();
+	void setColors();
+
+	void setupGui();
+	void displayProperties();
+	void display2D();
+	void display3D();
+
+	void btnSelectClicked();
+	void btnPropertyClicked();
+	void btn2DClicked();
+	void btn3DClicked();
+	void btnDrawClicked();
+	void btnExitClicked();
 
 };
