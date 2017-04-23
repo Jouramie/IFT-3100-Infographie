3720fc896b5a8491ace1f06fe3c921c7613ce714
Alexg <Alexgilbert@yahoo.com>
2017-04-23 19:15:55 -0400

    stuff and things

--- of_v0.9.8_vs_release/apps/ProjetInfographie_2/src/renderer.cpp
+++ of_v0.9.8_vs_release/apps/ProjetInfographie_2/src/renderer.cpp
@@ -516,7 +516,7 @@ ofParameter<bool> renderer::createCube(int x, int y, int z, int w, int h, int d,
 	primitive3d prim = primitive3d{ box, mat.getAmbientColor(), matrix };
 	prim.setName("Cube " + to_string(scn->nbElements() + 1));
 	prim.setMirror(false);
-	prim.setGlass(scn->nbElements() == 6);
+	prim.setGlass(false);
 	scn->addElement(prim);
 	return prim.selected;
 
